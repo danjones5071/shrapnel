d8a0917ee2e21552205cb363a84533ee7b88b272
danjones5071 <danjones5071@gmail.com>
2019-08-07 23:40:51 -0400

    SHRPX-9 Implement GET and GET:id for IssuesController

--- app/controllers/api/v1/issues_controller.rb
+++ app/controllers/api/v1/issues_controller.rb
@@ -1,4 +1,13 @@
 module Api::V1
   class IssuesController < ApplicationController
+    respond_to :json
+
+    def index
+      respond_with Issue.order(id: :DESC)
+    end
+
+    def show
+      respond_with Issue.find(params[:id])
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   root 'site#index'
+
+  namespace :api do
+    namespace :v1 do
+      resources :issues, only: %i[index show]
+    end
+  end
 end
