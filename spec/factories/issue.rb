f5e41ba77a0f5e623b9d77efbb0e605b66931b09
danjones5071 <danjones5071@gmail.com>
2019-08-10 20:40:05 -0400

    SHRPX-9 Use Faker in FactoryBot for better test attributes

--- spec/factories/issue.rb
+++ spec/factories/issue.rb
@@ -1,7 +1,11 @@
 FactoryBot.define do
   factory :issue do
-    issue_type { SecureRandom.uuid }
-    subject { SecureRandom.uuid }
-    issue_status { SecureRandom.uuid }
+    issue_type { Faker::Beer.brand }
+    issue_status { Faker::Beer.style }
+    subject { Faker::Movie.quote }
+    description{ Faker::Hacker.say_something_smart }
+    assignee { Faker::FunnyName.name }
+    estimate { Faker::Number.between(from: 1, to: 20) }
+    due_date { Faker::Date.forward }
   end
 end
